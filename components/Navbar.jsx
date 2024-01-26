a6a4f8a82a2f0659992b2c1ff4567c514816a973
FilipeMiranda22 <filipe.salomao@academico.ifpb.edu.br>
2024-01-26 14:39:55 -0300

    Updated api

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,9 @@ import { FaPlus } from "react-icons/fa";
 const Navbar = ({ handleShowModal }) => {
   return (
     <div className="flex w-auto md:w-[50vw] justify-between items-center m-auto bg-slate-200 p-5 mb-5 rounded-b-md">
-      <span className="text-lg sm:text-2xl font-semibold">DataManager</span>
+      <span className="text-lg sm:text-2xl font-semibold mr-4">
+        DataManager
+      </span>
       <Button pill onClick={handleShowModal}>
         Adicionar
         <FaPlus className="text-slate-100 ml-2" />
--- utils/api.js
+++ utils/api.js
@@ -1,1 +1,1 @@
-export const api = "http://localhost:3000/api";
+export const api = "http://localhost:3000/apicrud-mongo-db-test.vercel.app/api";
