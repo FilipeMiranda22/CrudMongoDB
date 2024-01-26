41cacac7b10a15902d55a90e3a9f9567ad4d9120
FilipeMiranda22 <filipe.salomao@academico.ifpb.edu.br>
2024-01-26 14:36:26 -0300

    Corrigindo tamanho da NavBar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaPlus } from "react-icons/fa";
 
 const Navbar = ({ handleShowModal }) => {
   return (
-    <div className="flex w-auto sm:w-[50-vw] justify-between items-center m-auto bg-slate-200 p-5 mb-5 rounded-b-md">
+    <div className="flex w-auto md:w-[50vw] justify-between items-center m-auto bg-slate-200 p-5 mb-5 rounded-b-md">
       <span className="text-lg sm:text-2xl font-semibold">DataManager</span>
       <Button pill onClick={handleShowModal}>
         Adicionar
